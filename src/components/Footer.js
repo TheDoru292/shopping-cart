94f330881fc0031e080b41d9b4016962297253f8
thedoru <thedoruv2@gmail.com>
2022-10-31 10:17:31 +0200

    Added components

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="sc-footer">
+      <p>Made by TheDoru</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="sc-header">
+      <h1>Waination</h1>
+    </div>
+  );
+};
+
+export default Header;
