7aee2417a7343076e9035115ac4b313abb3ec0db
TheDoru <thedoruv2@gmail.com>
2022-11-01 23:32:29 +0100

    item container

---
+++ src/components/Item.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import StarRatings from "react-star-ratings";
+
+function Item({ itemName, itemUrl, itemPrice, itemRating, itemId }) {
+  return (
+    <div className="top-item">
+      <Link className="top-item-link" to={`/product/${itemId}`}>
+        <img src={itemUrl} alt={itemName} />
+        <p className="top-item-name">{itemName}</p>
+        <div className="top-item-rating">
+          <StarRatings
+            rating={itemRating}
+            starDimension="15px"
+            starSpacing="2px"
+            starRatedColor="#f5c131"
+          />
+          <p>({itemRating})</p>
+        </div>
+        <p>${itemPrice}</p>
+      </Link>
+    </div>
+  );
+}
+
+export default Item;
