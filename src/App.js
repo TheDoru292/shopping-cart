1b6ed037201f7bd8770da609face6e8cf8dd48df
TheDoru <thedoruv2@gmail.com>
2022-11-01 11:25:17 +0100

    modify files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
-import Content from "./components/Content";
+import { Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Shop from "./components/Shop";
+import Product from "./components/Product";
+import NotFound from "./components/404";
+import About from "./components/About";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import "./assets/style.css";
 
 function App() {
+  const [itemsNumber, setItemsNumber] = useState(0);
+
+  const increment = () => {
+    setItemsNumber(itemsNumber + 1);
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Content />
+      <Header items={itemsNumber} />
+
+      <Routes>
+        <Route path="/" element={<Home cb={increment} />} />
+        <Route path="/shop" element={<Shop />}></Route>
+        <Route path="/about" element={<About />} />
+        <Route path="/product:id" element={<Product />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+
       <Footer />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
-const Header = () => {
+import { Link } from "react-router-dom";
+
+const Header = (props) => {
   return (
     <div className="sc-header">
-      <h1>Waination</h1>
+      <Link to="/">
+        <h1>Waination</h1>
+      </Link>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/shop">Shop</Link>
+        <Link to="/about">About Us</Link>
+      </nav>
+      <Link to="/cart">
+        <img src="./assets/cart.svg" alt="cart" />
+        <p>{props.items}</p>
+      </Link>
     </div>
   );
 };
