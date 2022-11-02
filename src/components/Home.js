1477b6b344c1df3ad83e0b04a5cda1800632d758
TheDoru <thedoruv2@gmail.com>
2022-11-02 12:32:47 +0100

    homepage finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,49 @@
-function Home() {
+import { useEffect, useState } from "react";
+import getItems from "../db/items";
+import { getRandomItems, sortByRatingUp } from "../modules/filter";
+import Item from "./Item";
+
+function Home(props) {
+  const [items, setItems] = useState(getItems());
+
+  useEffect(() => {
+    console.log(items);
+  }, [props.items, items]);
+
   return (
     <div className="sc-content">
-      <p>Welcome to our website!</p>
+      <h2>Our Top Items</h2>
+      <hr></hr>
+      <div className="items-container">
+        {sortByRatingUp(items, 5).map((item) => {
+          return (
+            <Item
+              itemName={item.name}
+              itemId={item.id}
+              itemPrice={item.price}
+              itemRating={item.rating}
+              itemUrl={item.url}
+            />
+          );
+        })}
+      </div>
+      <div className="might-container">
+        <h2>You might like</h2>
+        <hr></hr>
+        <div className="items-container">
+          {getRandomItems(items, 5).map((item) => {
+            return (
+              <Item
+                itemName={item.name}
+                itemId={item.id}
+                itemPrice={item.price}
+                itemRating={item.rating}
+                itemUrl={item.url}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
