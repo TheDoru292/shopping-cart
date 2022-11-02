d919c5b8114ea5bf66ca7beeb6dd64bfc1bde02d
TheDoru <thedoruv2@gmail.com>
2022-11-02 14:49:21 +0100

    cart shows user's item count

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const Header = (props) => {
+const Header = ({ cart }) => {
+  const [cartItems, setCartItems] = useState(0);
+
+  useEffect(() => {
+    if (cart === undefined) {
+      console.log("There are no items in cart!");
+    }
+    if (cart.length === 1) {
+      setCartItems(cart[0].quantity);
+    }
+    if (cart.length > 1) {
+      let newArray = cart;
+
+      setCartItems(
+        newArray.reduce((num, obj) => {
+          return num + parseInt(obj.quantity);
+        }, 0)
+      );
+    }
+  }, [cart]);
+
   return (
     <div className="sc-header">
       <Link className="sc-header-first" to="/">
@@ -12,8 +33,8 @@ const Header = (props) => {
         <Link to="/about">About Us</Link>
       </nav>
       <Link className="sc-header-last" to="/cart">
-        <img src="./assets/cart.svg" alt="cart" />
-        <p>{props.items}</p>
+        <img src="/assets/cart.svg" alt="cart" />
+        <p>{cartItems}</p>
       </Link>
     </div>
   );
